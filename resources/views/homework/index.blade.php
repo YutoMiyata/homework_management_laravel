5c258e0e503a75d1a044ae885364d035467170a5
YutoMiyata <yuto.miya525ta@gmail.com>
2023-02-28 14:36:15 +0900

    layout_base.htmlの作成

--- resources/views/homework/index.blade.php
+++ resources/views/homework/index.blade.php
@@ -8,28 +8,24 @@
     <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
 </head>
 <body>
-    <h1>一覧画面</h1>
+    <x-layout.base>
+        <h1>一覧画面</h1>
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>名前</th>
-            <th>詳細</th>
-        </tr>
-
-        @foreach($homeworks as $homework)
+        <table>
             <tr>
-                <td class="index_id">{{$homework->id}}</td>
-                <td class="index_name">{{$homework->name}}</td>
-                <td class="index_detail_button"><a href='{{route("homework.show",['id' => $homework->id])}}'>詳細</a></td>
+                <th>ID</th>
+                <th>名前</th>
+                <th>詳細</th>
             </tr>
-        @endforeach
-    </table>
 
-    {{-- <footer>
-        <div>
-            <a href="{{route('homework.create')}}">新規</a>
-        </div>
-    </footer> --}}
+            @foreach($homeworks as $homework)
+                <tr>
+                    <td class="index_id">{{$homework->id}}</td>
+                    <td class="index_name">{{$homework->name}}</td>
+                    <td class="index_detail_button"><a href='{{route("homework.show",['id' => $homework->id])}}'>詳細</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </x-layout.base>
 </body>
 </html>
